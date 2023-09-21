cc27a7a41e240fcb0e6cc62e9875a36fcab7d2ce
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-21 12:29:37 +0530

    API udates

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key: "process.env.API_KEY",
-    cx: process.env.CX_KEY,
+    // key: process.env.API_KEY,
+    // cx: process.env.CX_KEY,
+    key: "AIzaSyC3ITXNJAzvvTyDG8fl78Ehebj2C6j7c-E",
+    cx: "d23a5475a891a49d2",
 };
 // key: "YOUR_API_KEY",
 // cx: "YOUR_CX_KEY",
