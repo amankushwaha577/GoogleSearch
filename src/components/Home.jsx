a9f4f16a3be00cb407084ac80079c84d3b929be4
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-07-07 15:24:07 +0530

    UI changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <main className="grow flex justify-center">
                 <div className="w-full px-5 flex flex-col items-center mt-44">
                     <img
-                        className="w-[172px] md:w-[272px] mb-8"
+                        className="w-[172px] md:w-[400px] mb-8"
                         src={Logo}
                         alt="Logo"
                     />
